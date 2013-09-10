2e9525c0a3a35c1794f38186d9520d30e758adcb
Acanthostega <acanthostega@hotmail.fr>
2013-09-10 12:00:10 +0200

    New method for run commands on hosts. Each host wait at the end of ots list of commands to a signal saying that there is more jobs. When a coomand is well executed, a signal is send to the main program which wait for execution of commands in a channel with size of the list of commands

--- exec/exec_test.go
+++ exec/exec_test.go
@@ -1,11 +1,9 @@
 package exec
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/ElricleNecro/TOD/formatter"
-	"os/exec"
-	"strings"
+	color "github.com/daviddengcn/go-colortext"
 	"testing"
 )
 
@@ -21,7 +19,10 @@ func TestRunCommands(t *testing.T) {
 	// A list of host
 	hostnames := []string{
 		"chasselas.iap.fr",
-		//"vaccarese",
+		"vaccarese",
+		"carmenere",
+		"tokay",
+		"null",
 		//"carmenere",
 		//"babel",
 	}
@@ -31,7 +32,7 @@ func TestRunCommands(t *testing.T) {
 		Command: "/bin/hostname",
 		User:    user,
 	}
-	commands := make([]*formatter.Command, 5)
+	commands := make([]*formatter.Command, 20)
 	for i, _ := range commands {
 		commands[i] = command
 	}
@@ -40,76 +41,42 @@ func TestRunCommands(t *testing.T) {
 	hosts := make([]*formatter.Host, len(hostnames))
 	for i, host := range hostnames {
 
+		// new channel
+		channel := make(chan int)
+
 		// Create the host object in the slice
 		hosts[i] = &formatter.Host{
 			Hostname:    host,
 			Port:        22,
 			Protocol:    "tcp",
 			IsConnected: true,
+			Waiter:      &channel,
 		}
+
+		fmt.Println(host)
+		fmt.Println(hosts[i].Waiter)
 	}
 
 	// display
+	color.ChangeColor(color.Blue, false, color.None, false)
 	fmt.Println("All data initialized !")
 
 	// Dispatch commands on hosts
 	formatter.Dispatcher(
 		commands,
 		hosts,
+		true,
 	)
 
 	// display
+	color.ChangeColor(color.Blue, false, color.None, false)
 	fmt.Println("Dispatching the commands on hosts done !")
 
 	// Run commands in concurrent
-	RunCommands(hosts)
+	RunCommands(hosts, len(commands))
 
 	// display
+	color.ChangeColor(color.Blue, false, color.None, false)
 	fmt.Println("Commands done !")
-
-}
-
-func TestLaunchTask(t *testing.T) {
-	command := exec.Command("tr", "a-z", "A-Z")
-	command.Stdin = strings.NewReader("Un petit test")
-	var out bytes.Buffer
-	command.Stdout = &out
-
-	res := LaunchTask(command, nil)
-	if <-res {
-		t.Log(out.String())
-	}
-
-	if out.String() != "UN PETIT TEST" {
-		t.Error("Loupé, la commande a donnée : ", out.String(), " au lieu de ", "UN PETIT TEST")
-	}
-}
-
-// A simple test function for the range taking into account the new length
-// of a slice or not.
-func TestSlices(t *testing.T) {
-
-	// A slice for test
-	test := []int{1, 2, 3, 4, 5}
-
-	// loop over the slice
-	for i, bidule := range test {
-
-		if i == 2 {
-			test = append(test, 6, 7, 8, 9)
-		}
-
-		fmt.Println(bidule)
-	}
-
-	// with a simple loop
-	for i := 0; i < len(test); i++ {
-
-		if i == 2 {
-			test = append(test, 6, 7, 8, 9)
-		}
-
-		fmt.Println(test[i])
-	}
 
 }
