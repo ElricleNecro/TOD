ab89f76cca0ac290d7bd8910bda53f18f95823d5
Acanthostega <acanthostega@hotmail.fr>
2013-09-11 21:22:18 +0200

    Correct the bug for blocking send signal and the start of the loop after its initialization which started always at 0

--- exec/exec.go
+++ exec/exec.go
@@ -60,10 +60,6 @@ func RunCommands(hosts []*formatter.Host, ncommands int) {
 				false,
 			)
 
-			// change informations
-			host.CommandNumber = 0
-			host.Commands = make([]*formatter.Command, 0)
-
 			// display
 			color.ChangeColor(color.Green, false, color.None, false)
 			fmt.Println("Dispatching done for " + host.Hostname + " !")
@@ -82,115 +78,139 @@ func RunCommands(hosts []*formatter.Host, ncommands int) {
 		// Do an infinite loop for waiting when ended
 		for {
 
-			// display
-			color.ChangeColor(color.Magenta, true, color.Magenta, true)
-			fmt.Println("Executing list of commands for " + host.Hostname)
+			// check the size of commands to execute before
+			if len(host.Commands) != 0 {
 
-			// loop over commands on this hosts
-			for i := 0; i < len(host.Commands); i++ {
-
-				// number of the command
-				host.CommandNumber = i
-
-				// create a session object
-				session := ssh.New(
-					host.Commands[i].User,
-					host,
+				// display
+				color.ChangeColor(color.Blue, true, color.None, false)
+				fmt.Println(
+					"Executing ",
+					len(host.Commands),
+					" commands for "+host.Hostname,
 				)
 
-				// check the host can be called
-				if is, _ := checker.IsConnected(host); !is {
+				// loop over commands on this hosts
+				for i := host.CommandNumber; i < len(host.Commands); i++ {
 
-					// display
-					color.ChangeColor(color.Red, false, color.None, false)
-					fmt.Println("Can't connect to host " + host.Hostname)
+					// number of the command
+					host.CommandNumber = i
 
-					// dispatch remaining work to other hosts
-					disconnected <- host
-
-					// exit the loop
-					break loop
-				}
-
-				// Attempt a connection to the host
-				err := session.Connect()
-
-				// check the host can be called
-				if err != nil {
-
-					// display
-					color.ChangeColor(color.Red, false, color.None, false)
-					fmt.Println("Can't connect to host " + host.Hostname)
-
-					// dispatch remaining work to other hosts
-					disconnected <- host
-
-					// exit the loop
-					break loop
-				}
-
-				// add a session to connect to host
-				_, err = session.AddSession()
-				if err != nil {
-
-					// display
-					color.ChangeColor(color.Red, false, color.None, false)
-					fmt.Println(
-						"Problem when adding a session to the host !",
+					// create a session object
+					session := ssh.New(
+						host.Commands[i].User,
+						host,
 					)
+					fmt.Println(host)
+					fmt.Println(host.Commands[i])
 
-					// dispatch remaining work to other hosts
-					disconnected <- host
+					// check the host can be called
+					if is, err := checker.IsConnected(host); !is || (err != nil) {
 
-					// exit the loop
-					break loop
-				}
+						// display
+						color.ChangeColor(color.Red, false, color.None, false)
+						fmt.Println("Can't connect to host " + host.Hostname)
 
-				// execute the command on the host
-				output, err2 := session.Run(host.Commands[i].Command)
-				if err2 != nil {
+						// dispatch remaining work to other hosts
+						select {
+						case disconnected <- host:
+						default:
+						}
 
-					// display
-					color.ChangeColor(color.Red, false, color.None, false)
+						// exit the loop
+						break loop
+					}
+
+					// Attempt a connection to the host
+					err := session.Connect()
+
+					// check the host can be called
+					if err != nil {
+
+						// display
+						color.ChangeColor(color.Red, false, color.None, false)
+						fmt.Println("Can't connect to host " + host.Hostname)
+
+						// dispatch remaining work to other hosts
+						select {
+						case disconnected <- host:
+						default:
+						}
+
+						// exit the loop
+						break loop
+					}
+
+					// add a session to connect to host
+					_, err = session.AddSession()
+					if err != nil {
+
+						// display
+						color.ChangeColor(color.Red, false, color.None, false)
+						fmt.Println(
+							"Problem when adding a session to the host !",
+						)
+
+						// dispatch remaining work to other hosts
+						select {
+						case disconnected <- host:
+						default:
+						}
+
+						// exit the loop
+						break loop
+					}
+
+					// execute the command on the host
+					color.ChangeColor(color.Green, false, color.None, false)
 					fmt.Println(
-						"An error occurred during the execution " +
-							"of the command !",
+						"Execute command on " + host.Hostname,
 					)
-					fmt.Println(
-						"The command was: " + host.Commands[i].Command,
-					)
-					fmt.Println("Error information: " + err.Error())
+					output, err2 := session.Run(host.Commands[i].Command)
+					if err2 != nil {
 
-					// exit the loop
-					break loop
-				}
+						// display
+						color.ChangeColor(color.Red, false, color.None, false)
+						fmt.Println(
+							"An error occurred during the execution " +
+								"of the command !",
+						)
+						fmt.Println(
+							"The command was: " + host.Commands[i].Command,
+						)
+						fmt.Println("Error information: " + err.Error())
 
-				// The command has been executed correctly, say it to other
-				ender <- true
+						// exit the loop
+						break loop
+					}
 
-				// Close the session
-				session.Close()
+					// The command has been executed correctly, say it to other
+					ender <- true
 
-				// for now print the result of the command
-				color.ChangeColor(color.Magenta, false, color.None, false)
-				fmt.Println(output)
+					// Close the session
+					session.Close()
 
-				// wait here for new jobs
-				if i == len(host.Commands)-1 {
+					// for now print the result of the command
+					color.ChangeColor(color.Magenta, false, color.None, false)
+					fmt.Println(output)
 
-					// display
-					color.ChangeColor(color.Magenta, true, color.None, false)
-					fmt.Println("Waiting more jobs for " + host.Hostname)
+					// wait here for new jobs
+					if i == len(host.Commands)-1 {
 
-					// Now wait for new job
-					<-*(host.Waiter)
+						// display
+						color.ChangeColor(color.Magenta, true, color.None, false)
+						fmt.Println("Waiting more jobs for " + host.Hostname)
 
-					// display
-					color.ChangeColor(color.Magenta, true, color.None, false)
-					fmt.Println(host.Hostname + " has more jobs !")
-					color.ChangeColor(color.Green, true, color.None, false)
-					fmt.Println("Number of commands for " + host.Hostname + " :" +
-						strconv.Itoa(len(host.Commands)))
+						// Now wait for new job
+						<-*(host.Waiter)
+
+						// display
+						color.ChangeColor(color.Magenta, true, color.None, false)
+						fmt.Println(host.Hostname + " has more jobs !")
+						color.ChangeColor(color.Green, true, color.None, false)
+						fmt.Println("Number of commands for " + host.Hostname + " :" +
+							strconv.Itoa(len(host.Commands)))
+
+					}
 
 				}
 
@@ -202,9 +222,6 @@ func RunCommands(hosts []*formatter.Host, ncommands int) {
 
 	// loop over hosts and run the command
 	for _, host := range hosts {
-
-		fmt.Println(host.Hostname)
-		fmt.Println(&(*host))
 
 		// in several goroutine
 		go RunOnHost(hosts, host)
